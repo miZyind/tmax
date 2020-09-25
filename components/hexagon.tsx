0884c1f8040271c7d3b69a9ad0d61bf841da0e5f
miZyind <mizyind@gmail.com>
2020-09-25 17:50:12 +0700

    fix: styled attrs typings

--- components/hexagon.tsx
+++ components/hexagon.tsx
@@ -7,13 +7,13 @@ interface HexagonProps {
   height: number;
 }
 
-const Hexagon = styled.div.attrs(({ x, y, width, height }: HexagonProps) => ({
+const Hexagon = styled.div.attrs<HexagonProps>(({ x, y, width, height }) => ({
   style: {
     transform: `translate(${x}px, ${y}px)`,
     width,
     height,
   },
-}))`
+}))<HexagonProps>`
   position: absolute;
   background-color: rgba(1, 1, 1, 0.8);
   clip-path: polygon(0 25%, 50% 0, 100% 25%, 100% 75%, 50% 100%, 0 75%);
