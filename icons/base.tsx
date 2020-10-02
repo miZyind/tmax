5a43f1a4d3d33b050881457867eff800d9d44d56
miZyind <mizyind@gmail.com>
2020-10-02 18:07:59 +0700

    feat: update static resources

---
+++ icons/base.tsx
@@ -0,0 +1,23 @@
+import React, { ReactNode } from 'react';
+
+interface Props {
+  size: number;
+  color?: string;
+  children: ReactNode;
+}
+
+export type BaseIconProps = Pick<Props, 'size' | 'color'>;
+
+export default function BaseIcon({ size, color = 'white', children }: Props) {
+  return (
+    <svg
+      xmlns='http://www.w3.org/2000/svg'
+      width={size}
+      height={size}
+      fill={color}
+      viewBox='0 0 24 24'
+    >
+      {children}
+    </svg>
+  );
+}
--- theme/index.ts
+++ theme/index.ts
@@ -1,6 +1,9 @@
 import vars from './blueprint-vars.json';
 
-const theme = { vars };
+const theme = {
+  vars,
+  hexagon: 'polygon(0 25%, 50% 0, 100% 25%, 100% 75%, 50% 100%, 0 75%)',
+};
 
 export type Theme = typeof theme;
 
