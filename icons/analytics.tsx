7c7db829cfc62877299c9b97bf289abe2ebd87de
miZyind <mizyind@gmail.com>
2021-10-20 11:45:41 +0700

    feat: update analytics icon

--- icons/analytics.tsx
+++ icons/analytics.tsx
@@ -1,11 +1,10 @@
-import BaseIcon from './base';
+import { Icon } from '@blueprintjs/core';
 
 import type { BaseIconProps } from './base';
 
-export default function AnalyticsIcon(props: BaseIconProps) {
-  return (
-    <BaseIcon {...props}>
-      <path d='M1.019 13.019h3.849V24h-3.85zm8.943-6.68h3.85V24h-3.85zM19.132 0h3.85v24h-3.85z' />
-    </BaseIcon>
-  );
+export default function AnalyticsIcon({
+  size,
+  color = 'white',
+}: BaseIconProps) {
+  return <Icon icon='series-search' size={size} style={{ color }} />;
 }
