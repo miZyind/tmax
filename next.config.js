ba26f7c0d16ac62e18c5377f9a254e3d7e0ed6ab
miZyind <mizyind@gmail.com>
2021-11-08 22:20:16 +0700

    feat: integrate with Singularity

--- next.config.js
+++ next.config.js
@@ -1,4 +1,24 @@
 module.exports = {
   reactStrictMode: true,
   eslint: { ignoreDuringBuilds: true },
+  rewrites() {
+    return [
+      {
+        source: '/singularity',
+        destination: '/singularity/index.html',
+      },
+      {
+        source: '/wasm.js',
+        destination: '/singularity/wasm.js',
+      },
+      {
+        source: '/wasm_bg.wasm',
+        destination: '/singularity/wasm_bg.wasm',
+      },
+      {
+        source: '/assets/:path*',
+        destination: '/singularity/assets/:path*',
+      },
+    ];
+  },
 };
