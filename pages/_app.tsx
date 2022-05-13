8c3c02eb183b6f1ddfbfafdda42952893cc88b52
miZyind <mizyind@gmail.com>
2022-05-13 15:14:46 +0700

    feat: add GA

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,6 +4,7 @@ import '@blueprintjs/icons/lib/css/blueprint-icons.css';
 import '@blueprintjs/popover2/lib/css/blueprint-popover2.css';
 
 import Head from 'next/head';
+import Script from 'next/script';
 import { ThemeProvider, createGlobalStyle } from 'styled-components';
 import { SWRConfig } from 'swr';
 
@@ -20,12 +21,24 @@ const GlobalStyle = createGlobalStyle`
 const fetcher = (url: string) => fetch(url).then((response) => response.json());
 
 export default function App({ Component, pageProps }: AppProps) {
+  const GA_ID = process.env.NEXT_PUBLIC_GA_ID;
+
   return (
     <>
       <Head>
         <meta name='viewport' content='width=device-width, initial-scale=1.0' />
         <title>Turbulent miZyind at Xion</title>
       </Head>
+      <Script src={`https://www.googletagmanager.com/gtag/js?id=${GA_ID}`} />
+      <Script id='google-analytics'>
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){window.dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', '${GA_ID}');
+        `}
+      </Script>
       <GlobalStyle />
       <ThemeProvider theme={theme}>
         <SWRConfig value={{ fetcher }}>
