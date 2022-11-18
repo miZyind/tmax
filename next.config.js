02056fa500d4b02974638b9ab96ff11ec375e08d
miZyind <mizyind@gmail.com>
2022-11-18 17:11:55 +0700

    refactor: file structure of Singularity

--- next.config.js
+++ next.config.js
@@ -3,13 +3,4 @@ module.exports = {
   compiler: { styledComponents: true },
   eslint: { ignoreDuringBuilds: true },
   i18n: { locales: ['en'], defaultLocale: 'en' },
-  rewrites() {
-    return [
-      {
-        source: '/assets/:path*',
-        destination: '/singularity/assets/:path*',
-        locale: false,
-      },
-    ];
-  },
 };
